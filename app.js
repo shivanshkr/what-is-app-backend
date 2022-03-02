d272f01cc8c34042b5f44892143e967e85733dfd
Shivansh Kumar <shivansh.dbg@gmail.com>
2022-03-02 20:10:09 +0530

    config change

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const server = app.listen(PORT, () => {
 const io = require('socket.io')(server, {
   pingTimeout: 60000,
   cors: {
-    origin: 'http://localhost:4200',
+    origin: process.env.FRONTEND_URL,
   },
 })
 io.on('connection', (socket) => {
